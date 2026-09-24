7ba86d82acaf93b8dc2926df885ec31d308808f0
Ryo Yamagiwa <ryo.yamagiwa0250@gmail.com>
2026-09-24 20:49:12 +0900

    Hiltを連携するため、Applicationクラスを実装

--- app/src/main/java/ryo/myappcompany/fixingfieldsynclogger/FixingFieldSyncLoggerApplication.kt
+++ app/src/main/java/ryo/myappcompany/fixingfieldsynclogger/FixingFieldSyncLoggerApplication.kt
@@ -1,4 +1,19 @@
 package ryo.myappcompany.fixingfieldsynclogger
 
-class FixingFieldSyncLoggerApplication {
+import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+@HiltAndroidApp
+class FixingFieldSyncLoggerApplication : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workFactory)
+            .build()
 }
